a896ad9b8b661b4023b1d7b17abe9dbb317beb41
masayaomoto <masaya.omoto@di-v.co.jp>
2020-07-22 21:45:29 +0900

    header commit
    vue.jsを使用してheaderの作成
    li要素をクリックで該当するtextをtitleに表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <header>
+      <h1 class="logo">
+        TECH FLOW
+      </h1>
+      <div class="title">
+        {{title}}
+      </div>
+    </header>
+    <ul>
+      <li @click="title= 'test1'">test1</li>
+      <li @click="title= 'test2'">test2</li>
+      <li @click="title= 'test3'">test3</li>
+    </ul>
   </div>
 </template>
 
@@ -8,15 +20,35 @@
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      title: "test"
     }
   }
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+<style lang="scss">
+  header {
+    height: 100px;
+    width: 100vw;
+    background-color: rgb(77, 70, 70);
+    display: flex;
+    position: relative;
+    padding: 0px 20px;
+    .logo {
+      color: rgb(42, 218, 174);
+      font-size: 22px;
+      font-weight: bold;
+      line-height: 100px;
+    }
+    .title {
+      color: white;
+      font-size: 18px;
+      font-weight: bold;
+      line-height: 100px;
+      width: 100vw;
+      text-align: center;
+      position:absolute;
+      left: 0;
+    }
+  }
 </style>
