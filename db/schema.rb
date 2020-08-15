8f3ac3eb5d8aba2cc94dcf3da04e852901b891a2
ratovia <ratovia@hotmail.co.jp>
2020-08-15 13:24:08 +0900

    add end of input schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -17,3 +17,4 @@ ActiveRecord::Schema.define(version: 2020_08_13_014209) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+end
