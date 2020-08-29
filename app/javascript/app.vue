e32d888829e2a2f3565eb20f966e534d9a444e5d
harukitoda <haruki.toda@di-v.co.jp>
2020-08-29 12:26:57 +0900

    whitespace_adjustment

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -130,15 +130,17 @@ export default {
     }
     .child {
       width: 250px;
-      min-height: 50px;
+      min-height: 45px;
       line-height: 30px;
-      background-color: lightcoral;
-      padding: 15px 0px 0px;
+      background-color: #f17d5a;
+      color: white;
+      padding: 8px 0px 0px;
       margin-bottom: 20px;
       overflow: auto;
+      text-align: center;
       div {
         background-color:#fdfdff;
-        padding: 80px;
+        min-height: 200px;
         border: 1px solid lightgray
       }
     }
